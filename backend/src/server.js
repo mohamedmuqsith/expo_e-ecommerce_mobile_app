58962d3a76517c593fe0b1424330f18d2be76b8c
Mukshith <mohamedmukshidh35@gmail.com>
2026-02-04 22:50:37 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app=express();
+
+app.get("/api/health",(req,res)=>{
+  res.status(200).json({message:"succuess"})
+});
+
+app.listen(3000,()=>{
+    console.log("server is up and running")
+})
