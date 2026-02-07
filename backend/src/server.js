0fe5fe7f40edf9f67c38b2c2160eb8d64e8bd059
Mukshith <mohamedmukshidh35@gmail.com>
2026-02-08 00:09:39 +0530

    deployement config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-const app=express();
+const app = express();
 
-app.get("/api/health",(req,res)=>{
-  res.status(200).json({message:"succuess"})
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "success" });
 });
 
-app.listen(3000,()=>{
-    console.log("server is up and running")
-})
+app.listen(3000, () => {
+  console.log("Server is up and running");
+});
